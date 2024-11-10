173af2b427143c49c48951675accdcaa3089c6c8
Elaine Chao <34292064+elaine-ch@users.noreply.github.com>
2024-11-10 14:42:47 -0800

    Update JLUT.sv to use textfile

--- code/JLUT.sv
+++ code/JLUT.sv
@@ -2,12 +2,19 @@ module JLUT(
   input[7:0] Jptr,
   output logic[5:0] Jump);
 
-  always_comb case(Jptr)
-	0: Jump = 5; 
-	1: Jump = 3;
-	2: Jump = 6;
-	3: Jump = 8;
-	default: Jump = 0;
-  endcase
+ //  always_comb case(Jptr)
+	// 0: Jump = 5; 
+	// 1: Jump = 3;
+	// 2: Jump = 6;
+	// 3: Jump = 8;
+	// default: Jump = 0;
+ //  endcase
+
+logic[7:0] Core[64];
+
+initial 
+	$readmemb("jlut.txt",Core);
+
+always_comb Jump = Core[Jptr];
 
 endmodule
