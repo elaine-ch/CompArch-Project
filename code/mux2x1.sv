792b7975ec10ce60a6af05d5d94b3647c4e41ff9
Elaine Chao <34292064+elaine-ch@users.noreply.github.com>
2024-11-10 14:21:17 -0800

    Update mux2x1.sv

--- code/mux2x1.sv
+++ code/mux2x1.sv
@@ -2,13 +2,13 @@ module mux2x1 #(parameter WIDTH=8)(
   input logic [WIDTH-1:0] input0,
   input logic [WIDTH-1:0] input1,
   input logic sel,
-  output logic [WIDTH-1:0] output
+  output logic [WIDTH-1:0] out
 );
 
   always @(sel or input0) begin
     if(sel)
-      output = input1;
+      out = input1;
     else
-      output = input0;
+      out = input0;
   end
 endmodule
