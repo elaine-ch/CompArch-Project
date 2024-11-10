5dcd628ae07918f49f839cffa6c5f71ba8ebc54d
thanhnhanlam <71792534+thanhnhanlam@users.noreply.github.com>
2024-11-10 14:31:09 -0800

    Update var sizes

--- code/Ctrl.sv
+++ code/Ctrl.sv
@@ -1,26 +1,38 @@
 module Ctrl(
   input        [8:0] mach_code,
+  output logic [7:0] Jptr,
   output logic [2:0] Aluop,
-  output logic [1:0] Ra,
-                     Jptr,
-			         Rb,
-			         Wd,
-  output logic       WenR,
+  output logic [2:0] Ra,
+			            Rb,
+			            Wd,
+  output logic  WenR,
 					 WenD,
 					 Ldr,
 					 Str
 );
 
+//  always_comb begin
+//	Aluop = {1'b0, mach_code[1:0]};// ALU
+//	Jptr  = mach_code[3:2];		// jump pointer
+//	Ra	  = mach_code[5:4];		// reg file addr A
+//	Rb    = 2'b0;		      
+//	Wd    = mach_code[5:4];
+//	WenR  = mach_code[6];		// reg file write enable
+//	WenD  = !mach_code[6];		// data mem write enable
+//	Ldr   =	mach_code[7];		// load
   always_comb begin
-	Aluop = {1'b0, mach_code[1:0]};// ALU
-	Jptr  = mach_code[3:2];		// jump pointer
-	Ra	  = mach_code[5:4];		// reg file addr A
-	Rb    = 2'b0;		    
-	Wd    = mach_code[5:4];
+	// R type instructions
+	Aluop = mach_code[7:5]; // ALU
+	Ra	   = mach_code[4:3];	// operand reg A
+	Rb    = mach_code[2:1]; // operand reg B
+	// Rd    = mach_code[0];   // destination reg
+	
+	// B type instructions
+	Jptr  = mach_code[3:2]; // jump pointer
 	WenR  = mach_code[6];		// reg file write enable
 	WenD  = !mach_code[6];		// data mem write enable
 	Ldr   =	mach_code[7];		// load
-    Str	  = !mach_code[6];		// store
+   Str	  = !mach_code[6];		// store
 //    case(mach_code)
 
 //	endcase
