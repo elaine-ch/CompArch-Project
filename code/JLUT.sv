185d6fd9d61418509d8244d6979e4f484c1f3762
Elaine Chao <34292064+elaine-ch@users.noreply.github.com>
2024-11-10 16:12:38 -0800

    Update JLUT.sv

--- code/JLUT.sv
+++ code/JLUT.sv
@@ -2,14 +2,6 @@ module JLUT(
   input[7:0] Jptr,
   output logic[5:0] Jump);
 
- //  always_comb case(Jptr)
-	// 0: Jump = 5; 
-	// 1: Jump = 3;
-	// 2: Jump = 6;
-	// 3: Jump = 8;
-	// default: Jump = 0;
- //  endcase
-
 logic[7:0] Core[64];
 
 initial 
