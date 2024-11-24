2c19362814f6269bb9d28f343bba96872f1c62ef
thanhnhanlam <71792534+thanhnhanlam@users.noreply.github.com>
2024-11-23 18:32:40 -0800

    Update comment character to be #

--- Assembler/CommentRemover.java
+++ Assembler/CommentRemover.java
@@ -1,7 +1,7 @@
 public class CommentRemover implements Converter {
     public String process(String line) {
-        if (line.contains("//")) {
-            line = line.substring(0, line.indexOf("//"));
+        if (line.contains("#")) {
+            line = line.substring(0, line.indexOf("#"));
         }
         return line.trim();
     }
