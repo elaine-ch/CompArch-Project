4c0e5542120a15e4eca71a3f216722adb652cdbf
thanhnhanlam <71792534+thanhnhanlam@users.noreply.github.com>
2024-12-04 18:43:28 -0800

    Update R-type instructions' syntax

--- Assembler/Translator.java
+++ Assembler/Translator.java
@@ -2,7 +2,6 @@ public class Translator implements Converter {
     static int lineCount = 1;
 
     static final String[] R_TYPE_OPERANDS = new String[] {"r0", "r1", "r2", "r3"};
-    static final String[] R_TYPE_DESTINATION = new String[] {"r4", "r5"};
     static final String[] REGISTERS = new String[] {"r0", "r1", "r2", "r3", "r4", "r5", "r6", "r7"};
 
     public String process(String line) {
@@ -12,49 +11,48 @@ public class Translator implements Converter {
         
         String ins = "";
         switch(line.substring(0, 3)) {
+// ------------ R-type ops ------------
             case "and": 
                 ins += "0"; // R type
                 ins += "000"; // opcode
-                ins += getRegisters(line.substring(4), 3);
+                ins += getRRegisters(line.substring(4));
                 break;
             case "add": 
                 ins += "0"; // R type
                 ins += "001"; // opcode
-                ins += getRegisters(line.substring(4), 3);
+                ins += getRRegisters(line.substring(4));
                 break;
             case "sub": 
                 ins += "0"; // R type
                 ins += "010"; // opcode
-                ins += getRegisters(line.substring(4), 3);
+                ins += getRRegisters(line.substring(4));
                 break;
             case "orr": 
                 ins += "0"; // R type
                 ins += "011"; // opcode
-                ins += getRegisters(line.substring(4), 3);
+                ins += getRRegisters(line.substring(4));
                 break;
             case "lst": 
                 ins += "0"; // R type
                 ins += "100"; // opcode
-                ins += getRegisters(line.substring(4), 2);
-                ins += "1";
+                ins += getRRegisters(line.substring(4));
                 break;
-                case "rst": 
+            case "rst": 
                 ins += "0"; // R type
                 ins += "101"; // opcode
-                ins += getRegisters(line.substring(4), 2);
-                ins += "1";
+                ins += getRRegisters(line.substring(4));
                 break;
-                case "cmp": 
+            case "cmp": 
                 ins += "0"; // R type
                 ins += "110"; // opcode
-                ins += getRegisters(line.substring(4), 2);
-                ins += "1";
+                ins += getRRegisters(line.substring(4));
                 break;
             case "don": 
                 ins += "1"; // B type
                 ins += "111"; // opcode
                 ins += "11111"; // padding
                 break;
+// ------------ B-type ops ------------
             case "beq": 
                 ins = line.trim();
                 break;
@@ -99,9 +97,11 @@ public class Translator implements Converter {
     }
 
     /**
-     * Outputs an array of int corresponding to the registers' number
+     * Outputs an array of int corresponding to the R-type register operands
+     * @param line the assembly line containing comma-separated registers
+     * @return the 5-bit machine code corresponding to both R-type operands
      */
-    private String getRegisters(String line, int regNum) {
+    private String getRRegisters(String line) {
         // remove space/tabs, to lower case
         line = line.replace(" ", "");
         line = line.toLowerCase().trim();
@@ -109,34 +109,42 @@ public class Translator implements Converter {
         String[] parts = line.split(",");
 
         // check number of registers
-        if (parts.length != regNum) {
-            System.err.println("Invalid number of registers" + "at line " + lineCount);
+        if (parts.length != 2) {
+            System.err.println("Invalid number of registers at line " + lineCount);
         }
 
         String reg = "";
         reg += toBinary(assignRegister(parts[0], R_TYPE_OPERANDS), 2);
-        if (regNum >= 2) {
-            reg += toBinary(assignRegister(parts[1], R_TYPE_OPERANDS), 2);
-        }
-        if (regNum >= 3) {
-            reg += toBinary(assignRegister(parts[2], R_TYPE_DESTINATION), 1);
-        }
+        reg += toBinary(assignRegister(parts[1], REGISTERS), 3);
         return reg;
     }
 
+    /**
+     * Returns the register number (to be converted in binary) based on available registers
+     * @param reg the register to assign
+     * @param registers the available registers for a given operation
+     * @return the register number corresponding to the operation
+     */
     private int assignRegister(String reg, String[] registers) {
         for (int i = 0; i < registers.length; i++) {
             if (registers[i].equals(reg)) {
                 return i;
             }
         }
-        System.err.println("Assigned invalid register" + " at line " + lineCount);
+        System.err.println("Assigned invalid register value of: " + reg + "\n at line " + lineCount);
         return -1;
     }
 
+    /**
+     * Converts val from decimal to binary.
+     * Used for registers.
+     * @param val non-negative decimal value
+     * @param size number of bits to output
+     * @return the value converted to binary
+     */
     private String toBinary(int val, int size) {
         if (size < 0) {
-            System.err.println("Negative number" + " at line " + lineCount);
+            System.err.println("Negative number at line " + lineCount);
             return null;
         }
 
