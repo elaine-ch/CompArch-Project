d6c136f327671ad3eb581d3e2fb692604488bb44
Elaine Chao <elaine.iy.chao@gmail.com>
2024-12-13 19:08:40 -0800

    non working instruction rom

--- testbenches/InstrRom_tb.sv
+++ testbenches/InstrRom_tb.sv
@@ -1,6 +1,7 @@
 module InstrRom_tb();
 	logic[7:0] PC;
 	logic[8:0] mach_code;
+	logic[8:0] Core[256];
 
 	logic clk;
 
@@ -15,27 +16,29 @@ module InstrRom_tb();
 	initial begin				 // test sequence
 		clk = 0;
 		PC = 0;
+		
+		$readmemb("C:\Users\elain\Documents\useless\CSE-141L-2024\code\mach_code.txt", Core);
 
 		#10ns
-		$display("%b", PC);
+		$display("%d", Core[0]);
 		PC++;
 		#1ns
-		$display("%b", PC);
-
-		#10ns
-		PC++;
-		#1ns
-		$display("%b", PC);
+		$display("%d", Core[1]);
 
 		#10ns
 		PC++;
 		#1ns
-		$display("%b", PC);
+		$display("%d", Core[2]);
 
 		#10ns
 		PC++;
 		#1ns
-		$display("%b", PC);
+		$display("%d", Core[3]);
+
+		#10ns
+		PC++;
+		#1ns
+		$display("%d", Core[4]);
 
 		#10ns
 		$stop;
